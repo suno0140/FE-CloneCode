bd358367503f88d012ef84f1797bd631ecd559bd
suno Choi <vlfxhd@naver.com>
2022-12-24 13:19:46 +0900

    login 화면임시생성완료

---
+++ src/component/login/Screen.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoginPage = () => {
+  return (
+    <StContainer>
+      <StTitle>Login</StTitle>
+
+      <StDiv>
+        <Stlabel>
+          <span><img src="/_dj/img/s62_login_login_icon.jpg"></span>
+          <StInput></StInput>
+        </Stlabel>
+      </StDiv>
+    </StContainer>
+  );
+};
+
+export default LoginPage;
+
+const StContainer = styled.div`
+  min-width: 1150px;
+  padding: 0 0 0;
+  margin: 0 auto 0;
+  position: relative;
+`;
+
+const StTitle = styled.h2`
+  min-height: 30px;
+  padding: 0 0 34px;
+  text-align: center;
+  transition: all 0.5s;
+`;
+
+const StDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const Stlabel = styled.label`
+  clear: both;
+  overflow: hidden;
+  display: block;
+  width: 450px;
+  margin: 0 0 9px;
+  border: 1px solid #d9d9d9;
+  color: #8f8f91;
+`;
+
+const StInput = styled.input`
+  float: left;
+  width: 300px;
+  height: 45px;
+  font-size: 17px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  padding: 8px 2px 6px 3px;
+  border: 0 none;
+  color: #111;
+`;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "../pages/Login";
 
 const Router = () => {
-  return <div></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default Router;
