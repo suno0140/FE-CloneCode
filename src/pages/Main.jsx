34c392d5372286b6fa3194d9dabb73b97b8724c0
Suebin An <jejupalette@naver.com>
2022-12-24 14:43:58 +0900

    fix : headerScreen 명칭 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+import MainCarouselScreen from "../component/main/mainCarousel/screen/MainCarouselScreen";
 
 const Main = () => {
-  return <div>메인</div>;
+  return (
+    <Container>
+      <MainCarouselScreen />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default Main;
