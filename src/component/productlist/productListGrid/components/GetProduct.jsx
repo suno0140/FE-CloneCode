d5ff8d6f609ee416d3f8b0600c80a4a9576c9c5e
Suebin An <jejupalette@naver.com>
2022-12-26 19:12:13 +0900

    feat : 상품 리스트 페이지 연결

---
+++ src/component/productlist/productListGrid/components/GetProduct.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { __getProductList } from "../../../../redux/modules/productListSlice";
+import ItemScreen from "../../../common/item/ItemScreen";
+
+const GetProduct = () => {
+  const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  const category = searchParams.get("category");
+  const page = searchParams.get("page");
+  const productList = useSelector((state) => state.productList.productList);
+
+  useEffect(() => {
+    dispatch(__getProductList({ category, page }));
+  }, [dispatch, category, page]);
+  return productList.map((product) => (
+    <div key={product.name}>
+      <ItemScreen
+        mediumThumbnailImgUrl={product.mediumThumbnailImgUrl}
+        name={product.name}
+        price={product.price}
+        caption={product.caption}
+      />
+    </div>
+  ));
+};
+
+export default GetProduct;
