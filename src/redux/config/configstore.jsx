126fd94b5e4e92d5bf5fda0578b6099ab749f7ff
Suebin An <jejupalette@naver.com>
2022-12-27 19:53:14 +0900

    feat : header api 추가

--- src/redux/config/configstore.jsx
+++ src/redux/config/configstore.jsx
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import productList from "../modules/productListSlice";
 import newItems from "../modules/mainNewItemSlice";
 import signUpPost from "../modules/signUpSlice";
+import header from "../modules/headerSlice";
 
 const store = configureStore({
   reducer: {
     productList,
     newItems,
     signUpPost,
+    header,
   },
 });
 
