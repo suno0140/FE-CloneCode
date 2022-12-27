806abcfb0597df366e039b26e7b0eb1ee5443773
Suebin An <jejupalette@naver.com>
2022-12-27 09:24:20 +0900

    fix : hover 오류 수정

--- src/layout/header/components/LinkButton.jsx
+++ src/layout/header/components/LinkButton.jsx
@@ -1,31 +1,61 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import HoverBox from "./HoverBox";
 
 const LinkButton = ({
   linkName,
   linkTo,
   fontsize,
   margin,
-  hoveringFeature = false,
+  isHoverNeed = false,
 }) => {
-  const [isHovering, setIsHovering] = useState(false);
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const navigate = useNavigate();
   return (
     <>
-      <LinkButtonForm
-        margin={margin}
-        fontsize={fontsize}
-        onMouseOver={() => {
-          setIsHovering(true);
-        }}
-        onMouseDown={() => {
-          setIsHovering(false);
-        }}
-      >
-        <Link to={linkTo}>{linkName}</Link>
-        {/* <HoverBox /> */}
-        {/* {hoveringFeature & isHovering && <HoverBox />} */}
+      <LinkButtonForm margin={margin} fontsize={fontsize}>
+        <Link
+          to={linkTo}
+          onMouseOver={() => {
+            setIsMouseOver(true);
+          }}
+          onMouseOut={() => {
+            setIsMouseOver(false);
+          }}
+        >
+          {linkName}
+          {isHoverNeed && isMouseOver && (
+            <Container
+              onMouseOver={() => {
+                setIsMouseOver(true);
+              }}
+              onMouseOut={() => {
+                setIsMouseOver(false);
+              }}
+            >
+              <ul>
+                <li
+                  onClick={() => {
+                    navigate("/product/list?subcategory=iiin&page=1");
+                  }}
+                >
+                  iiin
+                </li>
+                <li
+                  onClick={() => {
+                    navigate("product/list?subcategory=finders&page=1");
+                  }}
+                >
+                  FINDERS
+                </li>
+              </ul>
+              <img
+                src="http://iiinjeju.com/_dj/img/category_item_img_1.jpg"
+                alt="상품 추천"
+              ></img>
+            </Container>
+          )}
+        </Link>
       </LinkButtonForm>
     </>
   );
@@ -37,9 +67,36 @@ const LinkButtonForm = styled.div`
   transition: all ease-in-out 0.2s;
   position: relative;
   a {
+    padding: 0.5rem;
     letter-spacing: 0.03rem;
     line-height: 1.5;
     font-weight: 400;
+  }
+`;
+
+const Container = styled.div`
+  position: absolute;
+  transform: translate(-3rem, 0.3rem);
+  display: flex;
+  justify-content: baseline;
+  background: white;
+  padding: 1rem 2rem 1rem 1rem;
+  border: 1px solid var(--color-black);
+  ul {
+    margin: 0;
+    padding: 0;
+    position: relative;
+    z-index: 3;
+    li {
+      text-decoration: none;
+      list-style: none;
+      padding-bottom: 0.5rem;
+    }
+  }
+  img {
+    width: 17rem;
+    position: relative;
+    left: 1rem;
   }
 `;
 
