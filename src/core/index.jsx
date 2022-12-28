b21208516167da3570666b2c9ca5ac265efb5655
Suebin An <jejupalette@naver.com>
2022-12-28 11:01:42 +0900

    feature: github, 카카오 로그인 정리 버튼 정리

--- src/core/index.jsx
+++ src/core/index.jsx
@@ -1,3 +1,4 @@
 // test server
 export const serverUrlApiV1 = process.env.REACT_APP_SERVER + "api/v1";
 export const githubId = process.env.REACT_APP_GITHUB_ID;
+export const kakaoId = process.env.REACT_APP_KAKAO_ID;
